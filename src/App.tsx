ab69790e0b431e9149b22624c68cc637f2319c79
HF_Home <hf77554@gmail.com>
2022-03-07 22:03:09 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainDisplay from './components/MainDisplay'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TIC-TAC-TOE</h1>
+      <MainDisplay />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainDisplay.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './mainDisplay.css'
+
+const MainDisplay = () => {
+  return (
+    <div className="grid-container">
+        <div className="grid-item" onClick={()=> console.log(1)}>
+            1
+        </div>
+        <div className="grid-item">2</div>
+        <div className="grid-item">3</div>  
+        <div className="grid-item">4</div>
+        <div className="grid-item">5</div>
+        <div className="grid-item">6</div>  
+        <div className="grid-item">7</div>
+        <div className="grid-item">8</div>
+        <div className="grid-item">9</div>  
+    </div>
+  )
+}
+
+export default MainDisplay

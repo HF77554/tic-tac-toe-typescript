2c7ee43033acfe158935b7af26da7780fd5968c1
Hugo <HF77554@gmail.com>
2022-03-11 12:13:17 -0500

    working on splitting logic from visual, creating logic folder

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,15 +1,21 @@
 import React, {useState} from 'react'
+import { PlayerType } from "./MainDisplay";
 import './grid.css'
 
-const Grid = () => {
+type Props = {
+    playersData: PlayerType;
+    onTurnMove: (clickedItem: number) => void;
+};
+
+const Grid: React.FC<Props> = ({ playersData, onTurnMove }) => {
   const gridId = [1,2,3,4,5,6,7,8,9]
 
   return (
     <div>
       <div className="grid-container">
         {gridId.map(id => 
-          <div key={id} className="grid-item" onClick={()=> console.log(id)}>
-            {id}
+          <div key={id} className="grid-item" onClick={() => onTurnMove(id)}>
+            
           </div>
         )}
       </div>
--- src/components/MainDisplay.tsx
+++ src/components/MainDisplay.tsx
@@ -1,13 +1,38 @@
 import {useState} from 'react'
 import Grid from './Grid'
 
+export type PlayerType = {
+  isPlayer1: boolean;
+  p1_GridSelect: Array<number>;
+  p2_GridSelect: Array<number>;
+};
+
 const MainDisplay = () => {
-  const [isPlayer1, playerTask] = useState(true)
+  const [playersData, playerTask] = useState<PlayerType>({
+    isPlayer1: true,
+    p1_GridSelect: [],
+    p2_GridSelect: []
+  })
+
+  const playerTurnHandler = (pressedGrid:number) => {
+    
+    if (playersData.isPlayer1){
+      const newGrid = [...playersData.p1_GridSelect, pressedGrid]
+      const newPlayerData = {...playersData, isPlayer1:!playersData.isPlayer1, p1_GridSelect:newGrid}
+      playerTask(newPlayerData)
+    } else {
+      const newGrid = [...playersData.p2_GridSelect, pressedGrid]
+      const newPlayerData = {...playersData, isPlayer1:!playersData.isPlayer1, p2_GridSelect:newGrid} 
+      playerTask(newPlayerData)
+    }
+    
+  }
 
   return (
     <div>
-      <h2>{isPlayer1 ? 'Player 1': 'Player 2'} turn</h2>
-      <Grid />
+      <h2>{playersData.isPlayer1 ? 'Player 1': 'Player 2'} turn</h2>
+      <Grid playersData={playersData} onTurnMove={playerTurnHandler}/>
+      <button onClick={()=>console.log(playersData)}>X</button>
     </div>
   )
 }
